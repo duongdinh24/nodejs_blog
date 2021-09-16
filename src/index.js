c35088f26c086b5a4e18945b3610e664e4efb37e
duongdinh24 <dinhmenhtn2412@gmail.com>
2021-09-16 20:47:57 +0700

    Refactor code: add routes and app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended: 'true'
+}));
+app.use(express.json());
+
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -18,13 +26,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-});
-
-// app.get('/news', (req, res) => {
-//   res.render('news')
-// });
+//Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
