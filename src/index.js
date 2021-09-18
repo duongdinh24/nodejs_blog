5963e970b1aa5c0b3116fcf3461a18ae20579a2c
duongdinh24 <dinhmenhtn2412@gmail.com>
2021-09-18 14:49:36 +0700

    fix command package.json

--- src/index.js
+++ src/index.js
@@ -20,18 +20,18 @@ app.use(express.json());
 app.use(morgan('combined'));
 
 // Template engine
-    app.engine(
+app.engine(
     'hbs',
     handlebars({
         extname: '.hbs',
     }),
 );
-    app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH: ', path.join(__dirname, 'resources/views'))
 
 //Route init
-    route(app);
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
